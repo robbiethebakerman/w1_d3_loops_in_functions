babe0f05fc3238008d5a520bf3416cc54db6cb9f
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-03 15:31:47 +0100

    Adds sum_array

--- my_functions.rb
+++ my_functions.rb
@@ -17,3 +17,19 @@
 def add_array_lengths(array_1, array_2)
   return array_1.concat(array_2).length
 end
+
+###################################
+
+# # METHOD 1 # #
+
+def sum_array(numbers)
+  sum = 0
+  for number in numbers
+    sum += number
+  end
+  return sum
+end
+
+###################################
+
+# # METHOD 1 # #
