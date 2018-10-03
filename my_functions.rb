ef50acf17fe522b78cb9cf4ef659fe5c60b193b8
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-03 15:27:09 +0100

    Initial commit

---
+++ my_functions.rb
@@ -0,0 +1,19 @@
+# # METHOD 1 ##
+
+# def add_array_lengths(array_1, array_2)
+#   array_1_length = array_1.length()
+#   array_2_length = array_2.length()
+#   return array_1_length + array_2_length
+# end
+
+# # METHOD 2 ##
+
+# def add_array_lengths(array_1, array_2)
+#   return array_1.length() + array_2.length()
+# end
+
+# # METHOD 3 ##
+
+def add_array_lengths(array_1, array_2)
+  return array_1.concat(array_2).length
+end
